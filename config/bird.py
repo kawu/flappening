211b1f07a98eae1dac6cc66da58dc2977ff65bc5
simon <simon@simons-MBP.fritz.box>
2020-02-01 21:23:58 +0100

    changes x position (stationary)

--- config/bird.py
+++ config/bird.py
@@ -6,7 +6,7 @@ bird = {
     #
     # size, position:
     'size': (30, 30),  # px
-    'startPosition': [30, 160],  # px
+    'startPosition': [120, 160],  # px
     #
     # physics:
     'startVelocity': 5.0,
