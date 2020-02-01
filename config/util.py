d53ec01cb94628f8ea48b5ccedcd3fceb3a1e1e8
simon <simon@simons-MBP.fritz.box>
2020-02-01 22:58:54 +0100

    added event and key mapping

--- config/util.py
+++ config/util.py
@@ -14,20 +14,22 @@ colors = {
 #
 # events:
 #
-events = {
+events = { \
     'QUIT': 0,  # : {'none'}
     'ACTIVEEVENT': 1,  # : {'gain', 'state'}
     'KEYDOWN': 2,  # : {'key', 'mod', 'unicode', 'scancode'}
     'KEYUP': 3,  # : {'key', 'mod'}
-}
+    'USEREVENT': 24,  # : {'key', 'mod'}
+          }
 
 #
 #
 # keys:
 #
-keys = {
+keys = { \
     'up': 273,
     'down': 274,
     'right': 275,
     'left': 276,
-}
+    'space': 32
+        }
