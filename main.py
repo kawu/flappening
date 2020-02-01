ec00735bc411ecfd1a8db2236997d80e55787d03
simon <simon@simons-MBP.fritz.box>
2020-02-01 22:01:10 +0100

    incorporated player

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
 
-from entities.bird import Bird
-from entities.util import Score
+from player.human import Human
 
 from config.game import game
-from config.util import colors, events, keys
+from config.util import colors
 
 
 class Game:
@@ -25,10 +24,10 @@ class Game:
     #
     def setup(self):
 
-        # init pygame
+        # initiate pygame
         pygame.init()
 
-        # set & save screen
+        # initiate & save screen
         self.screen = pygame.display.set_mode(game['size'])
 
         # add screen title
@@ -37,9 +36,8 @@ class Game:
         # initiate & save game clock
         self.clock = pygame.time.Clock()
 
-        # initiate player, score
-        self.bird = Bird(self.screen)
-        self.score = Score(self.screen, 0)
+        # initiate & save player
+        self.player = Human(self.screen, 0)
 
     #
     #
@@ -47,46 +45,24 @@ class Game:
     #
     def run(self):
 
-        while True:
+        gameOn: bool = True
 
-            # --- Main event loop
-            for event in pygame.event.get():
+        while gameOn:
 
-                flapped = False
+            # --- Player turn
+            if (self.player.turn() is False):
+                gameOn = False
 
-                # window close handler
-                if event.type == pygame.QUIT:
-
-                    # Close the window and quit.
-                    pygame.quit()
-
-                # game logic for keydown presses
-                if event.type == events['KEYDOWN']:
-
-                    # UP KEY
-                    if (event.key == keys['up']):
-                        flapped = True
-
-            self.bird.move(flapped=flapped)
-
-            self.score.increase()
-
-            # --- Screen-clearing code goes here
-            # If you want a background image, replace this clear with blit'ing
-            # background image.
+            # --- Screen-clearing
             self.screen.fill(colors['white'])
 
-            # --- Drawing code should go here
-            self.bird.draw()
-            self.score.draw()
+            # --- Draw player
+            self.player.draw()
 
-            # --- Go ahead and update the screen with what we've drawn.
+            # --- Update the screen
             pygame.display.flip()
 
-            if (not (self.bird.inBound())):
-                exit()
-
-            # --- Limit to 60 frames per second
+            # --- Update clock with game speed
             self.clock.tick(game['speed'])
 
 
