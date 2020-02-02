5952a9b48f604e95728ddb2a478436fd6c2dd78b
simon <simon@simons-MBP.fritz.box>
2020-02-02 11:31:21 +0100

    refractored main for testing

--- main.py
+++ main.py
@@ -1,75 +1,18 @@
-import pygame
-
-from player.human import Human
-from player.machine import Machine
-
-from config.game import game
-from config.util import colors
-
-
-class Game:
-
-    #
-    #
-    #  -------- Init -----------
-    #
-    def __init__(self):
-        super().__init__()
-
-        self.setup()
-        self.run()
-
-    #
-    #
-    # -------- Setup -----------
-    #
-    def setup(self):
-
-        # initiate pygame
-        pygame.init()
-
-        # initiate & save screen
-        self.screen = pygame.display.set_mode(game['size'])
-
-        # add screen title
-        pygame.display.set_caption(game['title'])
-
-        # initiate & save game clock
-        self.clock = pygame.time.Clock()
-
-        # initiate & save player(s)
-        self.player1 = Human(self.screen, 0)
-        self.player2 = Machine(self.screen, 0)
-
-    #
-    #
-    # -------- Game Loop -----------
-    #
-    def run(self):
-
-        gameOn: bool = True
-
-        while gameOn:
-
-            # --- Player turn(s)
-            if (self.player2.turn() is False):
-                gameOn = False
-
-            # --- Screen-clearing
-            self.screen.fill(colors['white'])
-
-            # --- Draw player(s)
-            self.player2.draw()
-
-            # --- Update the screen
-            pygame.display.flip()
-
-            # --- Update clock with game speed
-            self.clock.tick(game['speed'])
+# import the game
+from game import Game
 
 
 def main():
-    Game()
+
+    # create new Game Object
+    # gameMode = 0 : human player
+    # gameMode = 1 : single simple machine player
+    myGame = Game(gameMode=1)
+
+    # run the game
+    myGame.run()
 
 
-main()
+# run iff file is main
+if __name__ == '__main__':
+    main()
