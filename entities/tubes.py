391bdce5a68d6deb180cad1dc36166e1e1da89ce
simon <simon@simons-MacBook-Pro.local>
2020-02-05 17:35:14 +0100

    added distance variable

--- entities/tubes.py
+++ entities/tubes.py
@@ -24,18 +24,19 @@ class Tubes():
         self.speed: float = speed
 
         self.yCenter = self.rndYCenter()
+        self.xCenter = game['size'][0] - self.width
 
         # TODO cleaning
         self.upper = Particle(self.screen,
                               size=[self.width, self.yCenter - self.gap / 2],
-                              position=[600, 0],
+                              position=[self.xCenter, 0],
                               color=tubes['color'])
 
         # TODO cleaning
         self.lower = Particle(
             self.screen,
             size=[self.width, game['size'][1] - self.yCenter + self.gap / 2],
-            position=[600, self.yCenter + self.gap / 2],
+            position=[self.xCenter, self.yCenter + self.gap / 2],
             color=tubes['color'])
 
     def draw(self):
@@ -43,6 +44,10 @@ class Tubes():
         self.lower.draw()
 
     def move(self):
+        # move self center
+        self.xCenter -= self.speed
+
+        # move particles
         self.upper.position[0] -= self.speed
         self.lower.position[0] -= self.speed
 
@@ -55,5 +60,16 @@ class Tubes():
         # src: https://docs.python.org/2/library/random.html#random.randint
         return random.randint(minY, maxY)
 
+    def inBound(self):
+        # check if both particles are inBound:
+        if (self.upper.inBound() or self.lower.inBound()):
+            return True
+
+        else:
+            return False
+
     def getYCenter(self):
         return self.yCenter
+
+    def getXCenter(self):
+        return self.xCenter
