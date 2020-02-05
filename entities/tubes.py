c5dec7d5c721da67f5af782322d586ba080343ac
simon <simon@simons-MacBook-Pro.local>
2020-02-05 18:18:03 +0100

    commenting & type annotation

--- entities/tubes.py
+++ entities/tubes.py
@@ -23,27 +23,27 @@ class Tubes():
         self.gap: int = gap
         self.speed: float = speed
 
-        self.yCenter = self.rndYCenter()
-        self.xCenter = game['size'][0] - self.width
+        self.yCenter: float = self.rndYCenter()
+        self.xCenter: float = game['size'][0] - self.width
 
-        # TODO cleaning
+        # TODO solve this elegant
         self.upper = Particle(self.screen,
                               size=[self.width, self.yCenter - self.gap / 2],
                               position=[self.xCenter, 0],
                               color=tubes['color'])
 
-        # TODO cleaning
+        # TODO solve this elegant
         self.lower = Particle(
             self.screen,
             size=[self.width, game['size'][1] - self.yCenter + self.gap / 2],
             position=[self.xCenter, self.yCenter + self.gap / 2],
             color=tubes['color'])
 
-    def draw(self):
+    def draw(self) -> None:
         self.upper.draw()
         self.lower.draw()
 
-    def move(self):
+    def move(self) -> None:
         # move self center
         self.xCenter -= self.speed
 
@@ -51,7 +51,7 @@ class Tubes():
         self.upper.position[0] -= self.speed
         self.lower.position[0] -= self.speed
 
-    def rndYCenter(self):
+    def rndYCenter(self) -> float:
 
         minY: int = self.padding + self.gap / 2
         maxY: int = game['size'][1] - self.padding - self.gap / 2
@@ -60,16 +60,23 @@ class Tubes():
         # src: https://docs.python.org/2/library/random.html#random.randint
         return random.randint(minY, maxY)
 
-    def inBound(self):
-        # check if both particles are inBound:
+    def inBound(self) -> bool:
+        # check if both pipes are inBound:
         if (self.upper.inBound() or self.lower.inBound()):
             return True
 
-        else:
-            return False
+        # default False:
+        return False
 
-    def getYCenter(self):
+    def collision(self, particle) -> bool:
+        # check if on of the pipes are colliding:
+        if (self.upper.collision(particle) or self.lower.collision(particle)):
+            return True
+
+        return False
+
+    def getYCenter(self) -> float:
         return self.yCenter
 
-    def getXCenter(self):
+    def getXCenter(self) -> float:
         return self.xCenter
