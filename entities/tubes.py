37bbab1258af3349c2933a30519daecd83630265
simon <simon@simons-MacBook-Pro.local>
2020-02-05 17:16:08 +0100

    added movement and correct object placement

--- entities/tubes.py
+++ entities/tubes.py
@@ -1,30 +1,59 @@
+import random
+
 from entities.particle import Particle
 
 from config.tubes import tubes
+from config.game import game
 
 
 class Tubes():
     def __init__(self,
                  screen,
-                 width: float = tubes['width'],
-                 gap: float = tubes['gap'],
+                 width: int = tubes['width'],
+                 padding: int = tubes['padding'],
+                 gap: int = tubes['gap'],
                  speed: float = tubes['speed']):
 
         super().__init__()
 
         self.screen = screen
 
-        self.width: float = width
-        self.gap: float = gap
+        self.width: int = width
+        self.padding: int = padding
+        self.gap: int = gap
         self.speed: float = speed
 
-        self.upper = Particle(self.screen, color=tubes['color'])
-        self.lower = Particle(self.screen, color=tubes['color'])
+        self.yCenter = self.rndYCenter()
+
+        # TODO cleaning
+        self.upper = Particle(self.screen,
+                              size=[self.width, self.yCenter - self.gap / 2],
+                              position=[600, 0],
+                              color=tubes['color'])
+
+        # TODO cleaning
+        self.lower = Particle(
+            self.screen,
+            size=[self.width, game['size'][1] - self.yCenter + self.gap / 2],
+            position=[600, self.yCenter + self.gap / 2],
+            color=tubes['color'])
 
     def draw(self):
         self.upper.draw()
         self.lower.draw()
 
-    # TODO implement
     def move(self):
-        pass
+        self.upper.position[0] -= self.speed
+        self.lower.position[0] -= self.speed
+
+    def rndYCenter(self):
+
+        minY: int = self.padding + self.gap / 2
+        maxY: int = game['size'][1] - self.padding - self.gap / 2
+
+        # random.randint(min[px], max[px])
+        # src: https://docs.python.org/2/library/random.html#random.randint
+        return random.randint(minY, maxY)
+
+    def getYCenter(self):
+        return self.yCenter
