9accd04212f02da8ffcfbb305d0210fd8d41f791
simon <simon@simons-MBP.fritz.box>
2020-02-08 23:21:26 +0100

    added test training config

--- tests/training.py
+++ tests/training.py
@@ -9,6 +9,6 @@ def test__training():
 
     print('test running: machine training game play')
 
-    myGame = Game(gameMode=2, playerCount=1)
+    myGame = Game(gameMode=2, playerCount=70)
 
-    training(myGame, epochs=10)
+    training(myGame, epochs=15)
