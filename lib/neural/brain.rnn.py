f5ccab50623de4df8c2098f396367d8b7637dfd0
simon <simon@simons-MBP.fritz.box>
2020-02-08 21:45:37 +0100

    added simple brain

--- lib/neural/brain.rnn.py
+++ lib/neural/brain.rnn.py
@@ -15,6 +15,10 @@ class Brain(nn.Module):
         # save parameters
         self.brain_size = brain_size
 
+        # RNN
+        # https://pytorch.org/docs/stable/nn.html#torch.nn.RNN
+        self.fc1 = nn.RNN(2, self.brain_size)
+
         # Linear
         # https://pytorch.org/docs/stable/nn.html#torch.nn.Linear
         self.fc2 = nn.Linear(self.brain_size, 1)
@@ -29,10 +33,13 @@ class Brain(nn.Module):
     #
     def forward(self, x):
 
-        # apply Linear:
-        x = self.fc2(x)
+        # apply RNN with reshape x[n,m] -> x[n,1,m]:
+        x, _h = self.fc1(x.view(x.shape[0], 1, -1))
 
-        # apply Sigmoid:
+        # apply Linear with reshape x[n,1,m] -> x[n,m]:
+        x = self.fc2(x.view(x.shape[0], -1))
+
+        # apply Sigmoid
         x = self.ac1(x)
 
         return x
@@ -42,21 +49,15 @@ class Brain(nn.Module):
     #  -------- Think -----------
     #
     def think(self, data):
-        self.zero_grad()
 
-        x1 = torch.FloatTensor([data[0][0]])  # bird Y
-        x2 = torch.FloatTensor([data[1][0][0]])  # pipes.index(0) Y
-        x3 = torch.FloatTensor([data[1][0][1]])  # pipes.index(0) X
-
-        x = torch.cat((
-            x1,
-            x2,
-            x3,
-        ))
+        x = torch.cat(
+            (torch.FloatTensor([data[0]]), torch.FloatTensor(data[1])))
 
         x = self.forward(x)
 
-        if (False):  # x > .5
+        x = sum(x).item() / len(x)
+
+        if (x > .5):
             return True
 
         return False
