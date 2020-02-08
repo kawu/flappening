f45522510c875530eed1bc060b0fb2ded2e0f910
simon <simon@simons-MBP.fritz.box>
2020-02-08 14:33:43 +0100

    added information retrival

--- player/machine.py
+++ player/machine.py
@@ -10,17 +10,37 @@ class Machine(Player):
             score,
         )
 
+        self.inBirdPosition: list = []
+        self.inTubesPosition: list = []
+
+    def observe(self, tubes):
+
+        self.inBirdPosition = [
+            self.bird.position[0] / game['size'][0],
+            self.bird.position[1] / game['size'][1]
+        ]
+
+        for tube in tubes:
+            self.inTubesPosition.append([
+                tube.getXCenter() / game['size'][0],
+                tube.getYCenter() / game['size'][1]
+            ])
+
     # DUMMY:
     def interact(self, event):
 
         if (True):  # event.type == events['USEREVENT']
 
-            y = self.bird.position[1] / game['size'][1]
+            print(self.inBirdPosition)
+            print(self.inTubesPosition)
 
-            if (y > .4):
+            if (self.inBirdPosition[1] > .4):
                 return True
 
         return False
 
     def draw(self):
         super().draw()
+
+    def isMachine(self):
+        return True
