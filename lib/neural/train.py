f32f80d3351d0b0efdec9d3ceda11e1bee35c452
simon <simon@simons-MBP.fritz.box>
2020-02-09 12:24:21 +0100

    added reporting to training

--- lib/neural/train.py
+++ lib/neural/train.py
@@ -3,25 +3,34 @@ from .evolution import evolve
 
 def training(game, epochs: int = 10):
 
+    generation: list = []
+    nextGeneration: list = []
+
     for n in range(epochs):
 
+        # run game
         game.run()
 
-        players = game.getPlayersGarbage()
+        # save current generation, evolve the next
+        generation = game.getPlayersGarbage()
+        nextGeneration = evolve(generation)
 
-        nextPlayers = evolve(players)
-
-        freq = {}
-
-        for item in [player.getScore() for player in players]:
-
-            if (item in freq):
-                freq[item] += 1
-
-            else:
-                freq[item] = 1
-
-        print('Scores for epoch: \t' + str(n) + ' : ' + str(freq))
-
+        # reset, preload game
         game.reset()
-        game.loadPlayers(nextPlayers)
+        game.loadPlayers(nextGeneration)
+
+        # reporting
+        print('[Gen: %3d] \t avg: %4d \t best: %4d' %
+              (n + 1, avgScore(generation), generation[-1].getScore()))
+
+
+def avgScore(players):
+
+    SUM = 0
+
+    for item in [player.getScore() for player in players]:
+        SUM = SUM + item
+
+    AVG = SUM / len(players)
+
+    return AVG
