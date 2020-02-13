fa2083bbdc92944b5ea18dc9b9a7cfe2d8312a56
simon <simon@simons-MacBook-Pro.local>
2020-02-13 13:21:33 +0100

    added comments

--- flappening/neural/evolution.py
+++ flappening/neural/evolution.py
@@ -7,6 +7,7 @@ def evolve(players) -> list:
 
     for i in range(int(len(players) / 4)):
 
+        # TODO: improve bird selection, fitness evalutation
         newPlayers.append(mutate(players[-1]))
         newPlayers.append(mutate(players[-2]))
         newPlayers.append(mutate(players[-3]))
@@ -19,19 +20,19 @@ def mutate(player, rate: float = 0.02):
 
     newPlayer = player.copy()
 
+    # TODO: improve mutation with fitness
+
     for param in player.brain.parameters():
 
-        # Weights of linear layer
+        # mutate weights of linear layer
         if (len(param.shape) == 2):
             for i0 in range(param.shape[0]):
                 for i1 in range(param.shape[1]):
-
                     param[i0][i1] += rate * random.randint(-1, 1)
 
         # mutate biases of linear layer
         if (len(param.shape) == 1):
             for i0 in range(param.shape[0]):
-
                 param[i0] += rate * random.randint(-1, 1)
 
     return newPlayer
