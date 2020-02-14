fc5b4a8066bb3fef7edaf3d5a42cd9198989d96a
simon <simon@simons-MBP.fritz.box>
2020-02-14 20:08:42 +0100

    deleted test script

--- test.sh
+++
@@ -1,7 +0,0 @@
-make run trainEpochs=20 mutationRate=0.005  >> logs/output.txt
-make run trainEpochs=20 mutationRate=0.010  >> logs/output.txt
-make run trainEpochs=20 mutationRate=0.015  >> logs/output.txt
-make run trainEpochs=20 mutationRate=0.020  >> logs/output.txt
-make run trainEpochs=20 mutationRate=0.025  >> logs/output.txt
-make run trainEpochs=20 mutationRate=0.030  >> logs/output.txt
-make run trainEpochs=20 mutationRate=0.035  >> logs/output.txt
