0ea410779e813a6d426d5e718584b4ed549f88d8
simon <simon@simons-MBP.fritz.box>
2020-02-14 14:44:11 +0100

    avoid ZeroDivisionError

--- flappening/utils/calculate.py
+++ flappening/utils/calculate.py
@@ -3,6 +3,10 @@ def avgScore(players: list) -> float:
         calculates average player score.
     '''
 
+    # to avoid ZeroDivisionError
+    if (len(players) == 0):
+        return 0
+
     SUM: float = 0
 
     for item in [player.getScore() for player in players]:
